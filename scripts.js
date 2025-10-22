ffb46012780442eea179d0260c09a537f76b1cd1
winTIMOTHY <billybob889324@gmail.com>
2025-10-22 19:50:40 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+        function getNumber(){
+          document.getElementById(button)
+           let number = prompt('Chose a random number for me to guess');
+           alert('I guess the number: '+ number);
+        }
